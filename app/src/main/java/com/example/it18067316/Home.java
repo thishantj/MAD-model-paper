d36b4f9c5182af68b7452f2379848086de83390c
thishantj <thishantj@gmail.com>
2019-10-12 12:30:46 +0530

    Mad model paper interfaces

---
+++ app/src/main/java/com/example/it18067316/Home.java
@@ -0,0 +1,13 @@
+package com.example.it18067316;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/it18067316/ProfileManagement.java
@@ -0,0 +1,14 @@
+package com.example.it18067316;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_management);
+    }
+}
