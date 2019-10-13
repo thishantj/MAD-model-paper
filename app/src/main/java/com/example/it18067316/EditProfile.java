11ff913a91cecdf228f08c765df0ca8caf25019c
thishantj <thishantj@gmail.com>
2019-10-13 17:04:15 +0530

    radio button

--- app/src/main/java/com/example/it18067316/EditProfile.java
+++ app/src/main/java/com/example/it18067316/EditProfile.java
@@ -3,12 +3,38 @@ package com.example.it18067316;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class EditProfile extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    String gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        radioGroup = findViewById(R.id.rgender2);
+
+        radioGroup.setOnCheckedChangeListener((RadioGroup.OnCheckedChangeListener) this);
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.rbtnMale:
+                if (checked)
+                    gender="Male";
+                break;
+            case R.id.rbtnFemale:
+                if (checked)
+                    gender="Female";
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/it18067316/ProfileManagement.java
+++ app/src/main/java/com/example/it18067316/ProfileManagement.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import Database.DBHelper;
 
@@ -14,8 +16,10 @@ public class ProfileManagement extends AppCompatActivity {
 
     DBHelper db;
 
-    EditText username, dob, password, gender;
+    EditText username, dob, password;
+    String gender;
     Button update;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +32,34 @@ public class ProfileManagement extends AppCompatActivity {
         username = findViewById(R.id.txtusername2);
         dob = findViewById(R.id.txtdob);
         password = findViewById(R.id.txtPassword2);
+        radioGroup = findViewById(R.id.rgender);
 
         String unm = intent.getStringExtra(Home.USERNAME);
         String pwd = intent.getStringExtra(Home.PASSWORD);
 
         username.setText(unm);
         password.setText(pwd);
+        radioGroup.setOnCheckedChangeListener((RadioGroup.OnCheckedChangeListener) this);
     }
 
-    public void editProfile(View view){
-        Intent intent = new Intent(this, EditProfile.class);
-        startActivity(intent);
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.rbtnMale:
+                if (checked)
+                    gender="Male";
+                    break;
+            case R.id.rbtnFemale:
+                if (checked)
+                    gender="Female";
+                    break;
+        }
     }
+
+        public void editProfile (View view){
+            Intent intent = new Intent(this, EditProfile.class);
+            startActivity(intent);
+        }
 
 }
